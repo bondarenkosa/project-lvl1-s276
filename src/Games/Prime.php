6aa29722fb5e5206729a9122dec5b5efbf6eb1bf
Sergei Bondarenko <andbondsa@gmail.com>
2018-05-27 14:34:23 +0500

    fix game prime

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -22,6 +22,10 @@ function run()
 
 function isPrime(int $num)
 {
+    if ($num < 2) {
+        return false;
+    }
+
     for ($i = 2; $i <= $num / 2; $i++) {
         if ($num % $i === 0) {
             return false;
